93c929ae44b97d7924385d97b1a13c217e56927f
mok <mok0230@protonmail.com>
2022-01-13 21:52:24 -0800

    support telegram

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 declare module "slackify-markdown" {
-  function slackify(markdown: string, parseOption?: any): string;
+  function slackify(markdown: string, isTelegram?: boolean): string;
   export = slackify;
 }
