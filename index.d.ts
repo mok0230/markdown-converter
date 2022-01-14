ce37987c573ad6ee46f61e40df03f257c5e8d5f1
mok <mok0230@protonmail.com>
2022-01-13 22:04:46 -0800

    update typings to reflect markdown-converter

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module "slackify-markdown" {
-  function slackify(markdown: string, isTelegram?: boolean): string;
-  export = slackify;
+declare module "markdown-converter" {
+  function convertMarkdown(markdown: string, isTelegram?: boolean): string;
+  export = convertMarkdown;
 }
