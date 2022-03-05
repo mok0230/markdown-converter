73a95aa364324b2e05eabb54e0d962542d9adc55
mok <mok0230@protonmail.com>
2022-03-05 06:49:44 -0800

    DRY up code with wrapEntity

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,31 @@
-module.exports = {
-  wrap(string, ...wrappers) {
-    return [
-      ...wrappers,
-      string,
-      ...wrappers.reverse(),
-    ].join('');
-  },
+const phrasing = require('mdast-util-to-markdown/lib/util/container-phrasing');
 
-  isURL(string) {
-    try {
-      return Boolean(new URL(string));
-    } catch (error) {
-      return false;
-    }
-  },
+const wrap = (string, ...wrappers) => [
+  ...wrappers,
+  string,
+  ...wrappers.reverse(),
+].join('');
 
-  isPotentiallyEncoded(uri) {
-    return uri !== decodeURIComponent(uri || '');
-  },
+const isURL = string => {
+  try {
+    return Boolean(new URL(string));
+  } catch (error) {
+    return false;
+  }
 };
+
+const zeroWidthSpace = String.fromCharCode(0x200B);
+
+const isPotentiallyEncoded = uri => uri !== decodeURIComponent(uri || '');
+
+const wrapEntity = (entityType, marker, injectZeroWidthSpace) => (node, _parent, context) => {
+  // make headers to be just *strong*
+
+  const exit = context.enter(entityType);
+  const value = phrasing(node, context, { before: marker, after: marker });
+  exit();
+
+  return injectZeroWidthSpace ? wrap(value, zeroWidthSpace, marker) : wrap(value, marker);
+};
+
+module.exports = { wrap, isURL, isPotentiallyEncoded, wrapEntity };
