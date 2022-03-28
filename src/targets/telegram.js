db4feed0b467e6f0f8df26871d09972771314a58
mok <mok0230@protonmail.com>
2022-03-27 20:56:07 -0700

    escape problematic entities in telegram

--- src/targets/telegram.js
+++ src/targets/telegram.js
@@ -8,6 +8,18 @@ const createTelegramHandlers = (definitions, options) => ({
   emphasis: wrapEntity('emphasis', '_', false),
   // telegram API can fail in trying to parse content with asterisks
   listItem: replaceUlMarker,
+  text: (node, _parent, context) => {
+    const exit = context.enter('text');
+    // https://core.telegram.org/bots/api#markdown-style
+
+    const text = node.value.replace(/([_*`\\[])/g, '\\$1');
+    exit();
+
+    // Do we need more escaping like the default handler uses?
+    // https://github.com/syntax-tree/mdast-util-to-markdown/blob/main/lib/handle/text.js
+    // https://github.com/syntax-tree/mdast-util-to-markdown/blob/main/lib/unsafe.js
+    return text;
+  },
 });
 
 module.exports = { createTelegramHandlers };
