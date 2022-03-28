5c67b86bbe9870a06bff6b08dce1f976f2a87ee4
mok <mok0230@protonmail.com>
2022-03-27 21:35:58 -0700

    move text replace logic to util and extend to telegram

--- src/targets/slack.js
+++ src/targets/slack.js
@@ -1,6 +1,6 @@
 const phrasing = require('mdast-util-to-markdown/lib/util/container-phrasing');
 const {
-  wrap, isURL, isEncoded, wrapEntity, replaceUlMarker,
+  wrap, isURL, isEncoded, wrapEntity, replaceUlMarker, replaceTextCharacters,
 } = require('../utils');
 
 const createSlackHandlers = (definitions, options) => ({
@@ -70,21 +70,11 @@ const createSlackHandlers = (definitions, options) => ({
     return text ? `<${definition.url}|${text}>` : `<${definition.url}>`;
   },
 
-  text: (node, _parent, context) => {
-    const exit = context.enter('text');
-    // https://api.slack.com/reference/surfaces/formatting#escaping
-
-    const text = node.value
-      .replace(/&/g, '&amp;')
-      .replace(/<(?!@)/g, '&lt;')
-      .replace(/(?<!@[A-Z0-9]+)>/g, '&gt;');
-    exit();
-
-    // Do we need more escaping like the default handler uses?
-    // https://github.com/syntax-tree/mdast-util-to-markdown/blob/main/lib/handle/text.js
-    // https://github.com/syntax-tree/mdast-util-to-markdown/blob/main/lib/unsafe.js
-    return text;
-  },
+  text: replaceTextCharacters([
+    { searchValue: /&/g, newValue: '&amp;' },
+    { searchValue: /<(?!@)/g, newValue: '&lt;' },
+    { searchValue: /(?<!@[A-Z0-9]+)>/g, newValue: '&gt;' },
+  ]),
 });
 
 module.exports = { createSlackHandlers };
--- src/targets/telegram.js
+++ src/targets/telegram.js
@@ -1,5 +1,5 @@
 const {
-  wrapEntity, replaceUlMarker,
+  wrapEntity, replaceUlMarker, replaceTextCharacters,
 } = require('../utils');
 
 const createTelegramHandlers = (definitions, options) => ({
@@ -8,18 +8,8 @@ const createTelegramHandlers = (definitions, options) => ({
   emphasis: wrapEntity('emphasis', '_', false),
   // telegram API can fail in trying to parse content with asterisks
   listItem: replaceUlMarker,
-  text: (node, _parent, context) => {
-    const exit = context.enter('text');
-    // https://core.telegram.org/bots/api#markdown-style
-
-    const text = node.value.replace(/([_*`\\[])/g, '\\$1');
-    exit();
-
-    // Do we need more escaping like the default handler uses?
-    // https://github.com/syntax-tree/mdast-util-to-markdown/blob/main/lib/handle/text.js
-    // https://github.com/syntax-tree/mdast-util-to-markdown/blob/main/lib/unsafe.js
-    return text;
-  },
+  text: replaceTextCharacters([{ searchValue: /([_*`\\[])/g, newValue: '\\$1' }]),
+  // text: replaceTextCharacters(/this/g, 'lll'),
 });
 
 module.exports = { createTelegramHandlers };
