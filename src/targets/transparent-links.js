ad9cc5f07c5645fd55fef2dd1a4ba5bc249988a1
mok <mok0230@protonmail.com>
2022-04-19 13:54:13 -0700

    insert space before link in transparent-links

--- src/targets/transparent-links.js
+++ src/targets/transparent-links.js
@@ -6,16 +6,12 @@ const createTransparentLinksHandlers = (definitions, options) => ({
   link: (node, _parent, context) => {
     const { text, url } = getLinkParams(node, context, 'link');
 
-    console.log('type link');
-    console.log('text', text);
-    console.log('url', url);
-
     // mdast parses a link alone as both the text and url, in that case form a transparent link
     if (text === url) {
       return `[${url}](${url})`;
     }
 
-    return text ? `[${text}]([${url}](${url}))` : `[${url}](${url})`;
+    return text ? `${text} ([${url}](${url}))` : `[${url}](${url})`;
   },
 
   linkReference: (node, _parent, context) => {
@@ -28,13 +24,13 @@ const createTransparentLinksHandlers = (definitions, options) => ({
       if (!definition || !isURL(definition.url)) return `${text}`;
     }
 
-    return text ? `[${text}]([${definition.url}](${definition.url}))` : `[${definition.url}](${definition.url})`;
+    return text ? `${text} ([${definition.url}](${definition.url}))` : `[${definition.url}](${definition.url})`;
   },
 
   image: (node, _parent, context) => {
     const { text, url } = getImageParams(node, context, 'image', node.title);
 
-    return text ? `[${text}]([${url}](${url}))` : `[${url}](${url})`;
+    return text ? `${text} ([${url}](${url}))` : `[${url}](${url})`;
   },
 
   imageReference: (node, _parent, context) => {
@@ -42,7 +38,7 @@ const createTransparentLinksHandlers = (definitions, options) => ({
 
     const { text } = getImageParams(node, context, 'imageReference', definition ? definition.title : '');
 
-    return text ? `[${text}]([${definition.url}](${definition.url}))` : `[${definition.url}](${definition.url})`;
+    return text ? `${text} ([${definition.url}](${definition.url}))` : `[${definition.url}](${definition.url})`;
   },
 });
 
